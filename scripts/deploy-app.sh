a3005efcaa7d4e0c8f35c1262930f2ce0d19c6d6
Paulo Benatto <paulobenatto02@gmail.com>
2025-03-14 20:02:53 -0300

    build: Update deployment script to rebuild containers and add proxy network to docker-compose

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,5 +1,5 @@
 docker compose down
-docker compose up -d
+docker compose up -d --build
 
 docker compose exec django python manage.py migrate
 docker compose exec django python manage.py collectstatic --noinput
