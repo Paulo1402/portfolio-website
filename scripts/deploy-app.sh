a09a059715598f771e337007347859e813df0f87
Paulo Benatto <paulobenatto02@gmail.com>
2026-06-15 10:38:01 -0300

    ci(deploy): upload compose config to vps

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,5 +1,10 @@
-docker compose down
-docker compose up -d --build
+#!/usr/bin/env bash
 
-docker compose exec django python manage.py migrate
-docker compose exec django python manage.py collectstatic --noinput
+set -euo pipefail
+
+docker compose --env-file .env.cicd pull
+docker compose --env-file .env.cicd up -d
+
+docker compose --env-file .env.cicd exec -T django python manage.py migrate --noinput
+docker compose --env-file .env.cicd exec -T django python manage.py compilemessages
+docker compose --env-file .env.cicd exec -T django python manage.py collectstatic --noinput
